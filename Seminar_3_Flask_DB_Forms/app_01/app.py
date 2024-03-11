ebc1334bc560c5f0e0d4783a1c2862503d3256cc
Aleksandr <a.shaddar@mail.ru>
2024-03-11 22:10:06 +0300

    Add app to Seminar_3

--- Seminar_3_Flask_DB_Forms/app_01/app.py
+++ Seminar_3_Flask_DB_Forms/app_01/app.py
@@ -1,9 +1,21 @@
+# Создать базу данных для хранения информации о студентах университета.
+# База данных должна содержать две таблицы: "Студенты" и "Факультеты".
+# В таблице "Студенты" должны быть следующие поля: id, имя, фамилия,
+# возраст, пол, группа и id факультета.
+# В таблице "Факультеты" должны быть следующие поля: id и название
+# факультета.
+# Необходимо создать связь между таблицами "Студенты" и "Факультеты".
+# Написать функцию-обработчик, которая будет выводить список всех
+# студентов с указанием их факультета.
+
+
 from flask import Flask, render_template
-from models import db, User
+from models import db, Students, Fags, Gender
+from random import choice, randint
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app_1.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app_01.db'
 db.init_app(app)
 
 
@@ -15,8 +27,15 @@ def init_db():
 
 @app.cli.command("add-user")
 def add_user():
-    user = User(username='Alex', email='alex@mail.ru')
-    db.session.add(user)
+    for _ in range(1, 11):
+        fag = Fags(fag_name=choice(['math', 'hist', 'lang']))
+        db.session.add(fag)
+    db.session.commit()
+
+
+    for i in range(1, 11):
+        student = Students(name=f'name{i}', last_name=f'last_name{i}', age=i+15, gender=choice([Gender.male, Gender.female]), group=choice([1, 2, 3]), fags_id=randint(1, 10))
+        db.session.add(student)
     db.session.commit()
 
 
@@ -29,7 +48,8 @@ def del_user():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    student = Students.query.all()
+    return render_template('index.html', student=student)
 
 
 if __name__ == '__main__':
--- Seminar_3_Flask_DB_Forms/app_01/models.py
+++ Seminar_3_Flask_DB_Forms/app_01/models.py
@@ -1,15 +1,32 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+import enum
 
 db = SQLAlchemy()
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+class Gender(enum.Enum):
+    male = 'муж'
+    female = 'жен'
+
+
+class Fags(db.Model):
+    id_ = db.Column(db.Integer, primary_key=True)
+    fag_name = db.Column(db.String(80), nullable=False)
+    student = db.relationship('Students', backref=db.backref('fags'), lazy=True)
 
     def __repr__(self):
-        return f'User({self.username}, {self.email})'
+        return f'Fags({self.fag_name})'
+
+
+class Students(db.Model):
+    id_ = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.Enum(Gender), nullable=False)
+    group = db.Column(db.Integer, nullable=False)
+    fags_id = db.Column(db.Integer, db.ForeignKey('fags.id_'), nullable=False)
+
+    def __repr__(self):
+        return f'Student({self.name}, {self.last_name})'
