0bf6f6de179eb914c7e703556608e72a6796d057
Aleksandr <a.shaddar@mail.ru>
2024-03-14 21:47:38 +0300

    Add Seminar_4 and fix HW Seminar_3

--- Seminar_3_Flask_DB_Forms/homework/Task_8/forms.py
+++ Seminar_3_Flask_DB_Forms/homework/Task_8/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 
 class RegistrationForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     sername = StringField('Sername', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email(message='Введите корректный адрес!')])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
+    conf_password = PasswordField('ConfirmPassword', validators=[EqualTo('password', message='Пароли должны совпадать')])
