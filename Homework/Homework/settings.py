c3b0b19ff6417742f00df88f7608c50dd44e96a3
Aleksandr <a.shaddar@mail.ru>
2024-04-16 18:57:02 +0300

    Prepare to deploy

--- Homework/Homework/settings.py
+++ Homework/Homework/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 import logging
 
@@ -21,12 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v8x%8zry_zqg@&9lig3_io=qeyazg7$qe5nk2a)nd41x6tazq$'
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'AleksandrShaddar.pythonanywhere.com',
+]
 
 
 # Application definition
@@ -80,8 +85,15 @@ WSGI_APPLICATION = 'Homework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'AleksandrShaddar$default',
+        'USER': 'AleksandrShaddar',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'AleksandrShaddar.mysql.pythonanywhere-services.com'
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -121,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
